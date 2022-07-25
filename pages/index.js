8a85525c08c147ce67a2475c3e13cbacf0d6f78b
imsaifun <imsaifun@gmail.com>
2022-07-25 13:50:31 +0600

    scss and boorstrap added

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,45 @@
 import users from "../data/users.json";
 function Home() {
+    // const post = posts
 
-  return (
-    <>
-      {users.map((item, i) => (
+
+    // console.log(users);
+    return (
         <>
-          {item.user.username}
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-12">
+                        {users.map((item, i) => (
+                            <>
+                                {item.user.username}
+                                <br />
+
+                                {item.posts.map((post => (
+                                    <>
+                                        {post.title}
+                                        <br />
+                                        Likes : {post.likes}
+
+                                        <br />
+
+                                        {post.comments.map((comment) => (
+                                            <>
+
+                                                {comment.text}
+                                            </>
+                                        ))}
+                                    </>
+                                )))}
+                                <br />
+                            </>
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+
         </>
-      ))}
-    </>
-  )
+    )
 }
 export default Home;
