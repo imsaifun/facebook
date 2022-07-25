5f8185310dde7acd6db15c95dcd75bc18624f392
imsaifun <imsaifun@gmail.com>
2022-07-25 14:11:59 +0600

    post markup

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import users from "../data/users.json";
 function Home() {
     // const post = posts
@@ -9,31 +10,42 @@ function Home() {
 
             <div className="container">
                 <div className="row">
-                    <div className="col-xl-12">
-                        {users.map((item, i) => (
-                            <>
-                                {item.user.username}
-                                <br />
-
-                                {item.posts.map((post => (
-                                    <>
-                                        {post.title}
-                                        <br />
-                                        Likes : {post.likes}
-
-                                        <br />
-
-                                        {post.comments.map((comment) => (
+                    <div className="col-xl-6">
+                        <div className="posts">
+                            {users.map((item, i) => (
+                                <>
+                                    <div className="post-head">
+                                        <div className="d-flex align-items-center">
+                                            <img className="rounded-circle me-10" src={item.user.profile_picture} alt="" width="30" />
+                                            <strong> {item.user.username}</strong>
+                                        </div>
+                                    </div>
+                                    <div className="post-body">
+                                        {item.posts.map((post => (
                                             <>
+                                                <h4>{post.title}</h4>
+                                                <ul className="like-comment-share d-flex justify-content-between">
+                                                    <li>Likes : {post.likes}</li>
+                                                    <li>Comments</li>
+                                                    <li>Share</li>
+                                                </ul>
+                                                <div className="post-comment">
+                                                    {post.comments.map((comment) => (
+                                                        <>
 
-                                                {comment.text}
+                                                            {comment.text}
+                                                        </>
+                                                    ))}
+                                                </div>
                                             </>
-                                        ))}
-                                    </>
-                                )))}
-                                <br />
-                            </>
-                        ))}
+                                        )))}
+                                    </div>
+
+
+                                    <br />
+                                </>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
